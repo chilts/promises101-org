02c77834f5a0fca1c6b8baf2f64fc439cc371cfb
Andrew Chilton <andychilton@gmail.com>
2015-04-15 00:23:22 +1200

    Add better tests for read-json-file

--- test/read-json-file.js
+++ test/read-json-file.js
@@ -8,7 +8,7 @@ var test = require('tape')
 var readFile = require('./read-json-file/')
 
 test('read a file in', function(t) {
-  t.plan(5)
+  t.plan(9)
 
   var filename = path.join(__dirname, 'read-json-file', 'file-ok.json')
   var expected = {
@@ -16,16 +16,27 @@ test('read a file in', function(t) {
   }
 
   // plain
-  console.log(filename)
   readFile.plain(filename, function(err, data) {
     t.ok(!err, 'plain: there was no error')
     t.deepEqual(data, expected, 'plain: the data was correct')
+  })
+
+  // better
+  readFile.better(filename, function(err, data) {
+    t.ok(!err, 'better: there was no error')
+    t.deepEqual(data, expected, 'better: the data was correct')
   })
 
   // async
   readFile.async(filename, function(err, data) {
     t.ok(!err, 'async: there was no error')
     t.deepEqual(data, expected, 'async: the data was correct')
+  })
+
+  // asyncBetter
+  readFile.asyncBetter(filename, function(err, data) {
+    t.ok(!err, 'asyncBetter: there was no error')
+    t.deepEqual(data, expected, 'asyncBetter: the data was correct')
   })
 
   // bluebird
@@ -39,7 +50,7 @@ test('read a file in', function(t) {
 })
 
 test('read an invalid JSON file in', function(t) {
-  t.plan(5)
+  t.plan(9)
 
   var filename = path.join(__dirname, 'read-json-file', 'file-err.json')
   var errMsg = 'SyntaxError: Unexpected token m'
@@ -47,13 +58,25 @@ test('read an invalid JSON file in', function(t) {
   // plain
   readFile.plain(filename, function(err, data) {
     t.ok(err, 'plain: there an error parsing the JSON')
-    t.equal('' + err, errMsg, 'The error was correct')
+    t.equal('' + err, errMsg, 'plain: The error was correct')
+  })
+
+  // better
+  readFile.better(filename, function(err, data) {
+    t.ok(err, 'better: there an error parsing the JSON')
+    t.equal('' + err, errMsg, 'better: The error was correct')
   })
 
   // async
   readFile.async(filename, function(err, data) {
-    t.ok(err, 'plain: there an error parsing the JSON')
-    t.equal('' + err, errMsg, 'The error was correct')
+    t.ok(err, 'async: there an error parsing the JSON')
+    t.equal('' + err, errMsg, 'async: The error was correct')
+  })
+
+  // asyncBetter
+  readFile.asyncBetter(filename, function(err, data) {
+    t.ok(err, 'asyncBetter: there an error parsing the JSON')
+    t.equal('' + err, errMsg, 'asyncBetter: The error was correct')
   })
 
   // bluebird
@@ -61,13 +84,13 @@ test('read an invalid JSON file in', function(t) {
     .then(function(data) {
       t.fail('promise: there should have been an error parsing the JSON')
     }, function(err) {
-      t.equal('' + err, errMsg, 'The error was correct')
+      t.equal('' + err, errMsg, 'promise: The error was correct')
     })
   ;
 })
 
 test('read a non-existant file in', function(t) {
-  t.plan(5)
+  t.plan(9)
 
   var filename = path.join(__dirname, 'read-json-file', 'file-not-found.json')
   var errMsg = 'Error: ENOENT, open '
@@ -78,10 +101,22 @@ test('read a non-existant file in', function(t) {
     t.equal(('' + err).substr(0, 20), errMsg, 'plain: The error was correct')
   })
 
+  // better
+  readFile.better(filename, function(err, data) {
+    t.ok(err, 'better: there an error opening the file')
+    t.equal(('' + err).substr(0, 20), errMsg, 'better: The error was correct')
+  })
+
   // async
   readFile.async(filename, function(err, data) {
-    t.ok(err, 'plain: there an error opening the file')
+    t.ok(err, 'async: there an error opening the file')
     t.equal(('' + err).substr(0, 20), errMsg, 'async: The error was correct')
+  })
+
+  // asyncBetter
+  readFile.asyncBetter(filename, function(err, data) {
+    t.ok(err, 'asyncBetter: there an error opening the file')
+    t.equal(('' + err).substr(0, 20), errMsg, 'asyncBetter: The error was correct')
   })
 
   // bluebird
--- test/read-json-file/index.js
+++ test/read-json-file/index.js
@@ -20,6 +20,23 @@ module.exports.plain = function plain(filename, callback) {
   })
 }
 
+function parse(data, callback) {
+  try {
+    var v = JSON.parse(data)
+    process.nextTick(callback.bind(null, null, v))
+  }
+  catch (e) {
+    process.nextTick(callback.bind(null, e))
+  }
+}
+
+module.exports.better = function plain(filename, callback) {
+  fs.readFile(filename, function(err, data) {
+    if (err) return callback(err)
+    parse(data, callback)
+  })
+}
+
 module.exports.async = function plain(filename, callback) {
   async.waterfall(
     [
@@ -34,6 +51,16 @@ module.exports.async = function plain(filename, callback) {
           done(e)
         }
       }
+    ],
+    callback
+  )
+}
+
+module.exports.asyncBetter = function plain(filename, callback) {
+  async.waterfall(
+    [
+      fs.readFile.bind(fs, filename, 'utf8'),
+      parse.bind(null)
     ],
     callback
   )
